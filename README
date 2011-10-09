1075e859706ae3c6be25b75e8c02c4e0cd3489e9
Frol <frol@ewadev.com>
2011-10-09 17:33:00 +0300

    initial

---
+++ README
@@ -0,0 +1,41 @@
+cropresize
+==========
+
+cropresize uses `PIL <http://www.pythonware.com/products/pil/>`_
+to crop and resize an image as appropriate for web
+presentation. cropresize is a convenience package that allows image
+resizing without aspect ratio distortion.
+
+API
+---
+
+cropresize contains one useful function, ``cropresize.crop_resize``.
+The function takes three arguments:
+
+    * image: a `PIL image <http://www.pythonware.com/library/pil/handbook/image.htm>`_ object
+    * size: a 2-tuple of (width,height);  at least one must be specified
+    * exact_size: whether to scale up for smaller images  
+
+See ``cropresize.crop_resize.__doc__`` for the function
+documentation.  ``crop_resize`` returns the cropped and resized PIL image.
+
+
+Command Line
+------------
+
+The command line program, ``crop-resize``, is included in this python
+package.  The help for the program is displayed by running
+``crop-resize`` with no arguments or ``crop-resize --help``.
+
+
+Future Work
+-----------
+
+cropresize is written as a convenience function to PIL as this
+methodology for cropping and resizing images is often desired for
+presentation, particularly on the web.  Since the utility is in
+demand, the functionality should be ported upstream to PIL.
+
+--
+
+http://k0s.org/portfolio/software.html#cropresize
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from pkg_resources import require, DistributionNotFound
+import os
+
+try:
+    filename = os.path.join(os.path.dirname(__file__), 'README')
+    description = file(filename).read()
+except:
+    description = ''
+
+# Dependency check at run time
+# If PIL is not found, then it is added in the ``install_requires`` list
+install_requires = []   # Empty list if PIL is found
+try:
+    try:
+        require('PIL')
+    except DistributionNotFound:
+        require('Image')
+except DistributionNotFound:
+    install_requires = ['PIL']
+
+version = '0.1.6'
+
+setup(name='cropresize',
+      version=version,
+      description="crop and resize an image without doing the math yourself",
+      long_description=description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='image',
+      author='Jeff Hammel',
+      author_email='k0scist@gmail.com',
+      url='http://pypi.python.org/pypi/cropresize',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      crop-resize = cropresize:main
+      """,
+      )
