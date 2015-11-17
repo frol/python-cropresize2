9629f02725f60e3c50013ef4e151bde8c2cecca6
Vlad Frolov <frolvlad@gmail.com>
2015-11-17 10:57:41 +0700

    Added support for the case when only one size dimention is set (and the other is None); Returned support for exact_size (but enabled by default as it used to work this way before this support is returned); Improved overall code quality (pylint 10 out of 10)

--- cropresize2/__init__.py
+++ cropresize2/__init__.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+cropresize2 uses `Pillow <https://python-pillow.github.io/>`_ to crop and
+resize an image as appropriate for web presentation. cropresize2 is a
+convenience package that allows image resizing without aspect ratio distortion.
+"""
 
 import sys
 try:
@@ -9,7 +14,7 @@ except ImportError:
 # Crop modes
 CM_AUTO, CM_FORCECROP, CM_NOCROP = range(3)
 
-def crop_resize(image, size, exact_size=False, crop_mode=CM_AUTO):
+def crop_resize(image, size, exact_size=True, crop_mode=CM_AUTO):
     """
     Crop out the proportional middle of the image and set to the desired size.
     * image: a PIL image object
@@ -17,46 +22,68 @@ def crop_resize(image, size, exact_size=False, crop_mode=CM_AUTO):
     * exact_size: whether to scale up for smaller images
     If the image is bigger than the sizes passed, this works as expected.
     If the image is smaller than the sizes passed, then behavior is dictated
-    by the ``exact_size`` flag.  If the ``exact_size`` flag is false,
-    the image will be returned unmodified.  If the ``exact_size`` flag is true,
+    by the ``exact_size`` flag.  If the ``exact_size`` flag is False,
+    the image will be returned unmodified.  If the ``exact_size`` flag is True,
     the image will be scaled up to the required size.
     """
     assert size[0] or size[1], "Must provide a width or a height"
 
     size = list(size)
 
-    image_ar = image.size[0]/float(image.size[1])
-    new_image_ar = size[0]/float(size[1])
-    _crop = (crop_mode == CM_FORCECROP) or \
-            (crop_mode == CM_AUTO and ((new_image_ar / 2 > image_ar) or (new_image_ar * 2 < image_ar)))
+    image_aspect_ratio = image.size[0] / float(image.size[1])
 
-    if _crop:
-        if image_ar > new_image_ar:
+    if not size[0]:
+        size[0] = size[1] * image_aspect_ratio
+    elif not size[1]:
+        size[1] = size[0] / image_aspect_ratio
+
+    if not exact_size and tuple(size) >= image.size:
+        return image
+
+    new_image_aspect_ratio = size[0] / float(size[1])
+
+    crop_is_required = (
+        (crop_mode == CM_FORCECROP) or \
+        (crop_mode == CM_AUTO and (
+            (new_image_aspect_ratio / 2 > image_aspect_ratio)
+            or
+            (new_image_aspect_ratio * 2 < image_aspect_ratio)
+        ))
+    )
+
+    if crop_is_required:
+        if image_aspect_ratio > new_image_aspect_ratio:
             # trim the width
-            xoffset = int(0.5*(image.size[0] - new_image_ar*image.size[1]))
+            xoffset = int(0.5*(image.size[0] - new_image_aspect_ratio*image.size[1]))
             image = image.crop((xoffset, 0, image.size[0]-xoffset, image.size[1]))
-        elif image_ar < new_image_ar:
+        elif image_aspect_ratio < new_image_aspect_ratio:
             # trim the height
-            yoffset = int(0.5*(image.size[1] - image.size[0]/new_image_ar))
+            yoffset = int(0.5*(image.size[1] - image.size[0]/new_image_aspect_ratio))
             image = image.crop((0, yoffset, image.size[0], image.size[1] - yoffset))
     else:
-        if image_ar > new_image_ar:
+        if image_aspect_ratio > new_image_aspect_ratio:
             size[1] = int(image.size[1]*size[0]/float(image.size[0]))
         else:
             size[0] = int(image.size[0]*size[1]/float(image.size[1]))
 
+    size = (int(size[0]), int(size[1]))
+    if size == image.size:
+        return image
     return image.resize(size, Image.ANTIALIAS)
 
 def main():
+    """
+    Command line entry point function.
+    """
     from optparse import OptionParser
     parser = OptionParser('%prog [options] image1.png [image2.jpg] [...]')
     parser.add_option('-W', '--width',
                       help="desired width of image in pixels")
     parser.add_option('-H', '--height',
                       help="desired height of image in pixels")
-    parser.add_option('-e', '--exact-size', dest='exact',
+    parser.add_option('-e', '--no-exact-size', dest='no_exact_size',
                       action='store_true', default=False,
-                      help="scale up images smaller than specified")
+                      help="don't scale up images smaller than specified")
     parser.add_option('-d', '--display', dest='display',
                       action='store_true', default=False,
                       help="display the resized images (don't write to file)")
@@ -90,7 +117,12 @@ def main():
     # resize the images
     for arg in args:
         image = Image.open(arg)
-        new_image = crop_resize(image, (width, height), options.exact, options.force_crop and CM_FORCECROP or CM_AUTO)
+        new_image = crop_resize(
+            image,
+            size=(width, height),
+            exact_size=not options.no_exact_size,
+            crop_mode=CM_FORCECROP if options.force_crop else CM_AUTO
+        )
         if options.display:
             new_image.show()
         else:
@@ -99,7 +131,7 @@ def main():
                 if options.output:
                     new_image.save(options.output)
                 else:
-                    sys.stdout.write(new_image.tostring(image.format))
+                    sys.stdout.write(new_image.tobytes(image.format))
 
 if __name__ == '__main__':
     main()
